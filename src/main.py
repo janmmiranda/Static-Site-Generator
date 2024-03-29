062c38933756b011f22d289db41097ca4b9194ba
janmmiranda <janm.miranda@gmail.com>
2024-03-29 17:02:14 -0400

    create textnode and unit tests

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+print("Hello World")
+
+def main():
+    test = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(repr(test))
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,18 @@
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, text_node):
+        if (
+            self.text == text_node.text and
+            self.text_type == text_node.text_type and
+            self.url == text_node.url
+        ):
+            return True
+        else:
+            return False
+        
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
