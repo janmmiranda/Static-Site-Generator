8a9b306d0f8dbf4805a40185171984356d56e7a8
janmmiranda <janm.miranda@gmail.com>
2024-03-30 16:28:44 -0400

    add inline_markdown.py with split_nodes_delimiter(). added test cases for this

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,32 +2,33 @@ import unittest
 
 from textnode import TextNode
 from htmlnode import LeafNode
+from text_type_enum import TextType
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node", TextType.bold.name)
+        node2 = TextNode("This is a text node", TextType.bold.name)
         self.assertEqual(node, node2)
 
     def test_no_eq(self):
-        node = TextNode("This is a text node", "italic")
-        node2 = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node", TextType.italic.name)
+        node2 = TextNode("This is a text node", TextType.bold.name)
         self.assertNotEqual(node, node2)
 
     def test_eq_url(self):
-        node = TextNode("This is a text node", "bold", "www.test.com")
-        node2 = TextNode("This is a text node", "bold", "www.test.com")
+        node = TextNode("This is a text node", TextType.link.name, "www.test.com")
+        node2 = TextNode("This is a text node", TextType.link.name, "www.test.com")
         self.assertEqual(node, node2)
 
     def test_convert_to_html_text(self):
-        text_node = TextNode("Text", "text")
+        text_node = TextNode("Text", TextType.text.name)
         html_node = text_node.text_node_to_html_node(text_node)
         leaf_node = LeafNode(None, "Text")
         self.assertEqual(repr(html_node), repr(leaf_node))
 
     def test_convert_to_html_link(self):
-        text_node = TextNode("link", "link", "www.link.com")
+        text_node = TextNode("link", TextType.link.name, "www.link.com")
         html_node = text_node.text_node_to_html_node(text_node)
         leaf_node = LeafNode("a", "link",  {"href": "www.link.com"})
         self.assertEqual(repr(html_node), repr(leaf_node))
